70e9f3b3eac865ee58061b4d75df7ae9bbad574c
Scott_Xu <scottxu@stanford.edu>
2021-04-07 16:03:51 +0800

    Add implementation for dataloader and model demo

---
+++ automl_workflow/cifar10_data_loader.py
@@ -0,0 +1,39 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import backbone_model
+import torchvision.models as models
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+'''
+If needed to download, change to download=True
+'''
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=False)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+# net = backbone_model.MyBackboneModel1()
+# net = models.resnet18(pretrained=True)
+net = backbone_model.MyBackboneModel3()
+print(net)
+
+for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        output = net(inputs)
+        print(output)
+        break
